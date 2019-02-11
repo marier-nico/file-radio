4e2e73552f4db19d888f8f269a3f93f804c68f88
Charles <Charles@192.168.2.17>
2019-02-10 20:53:29 -0500

    Ajout de méthodes gérant l'action des boutons select. et envoyer

--- src/vues/ControleurVueEmetteur.java
+++ src/vues/ControleurVueEmetteur.java
@@ -1,14 +1,22 @@
 package vues;
 
 import controleurs.ApplicationRadio;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 
 public class ControleurVueEmetteur {
-	
+
 	@FXML
-    private BorderPane borderPaneRoot;
-	
+	private BorderPane borderPaneRoot;
+
+	@FXML
+	private Button btnSelectionner;
+
+	@FXML
+	private Button btnEnvoyer;
+
 	private ApplicationRadio application = null;
 	public static final String ADRESSE_VUE_EMETTEUR = "/vues/Vue_Emetteur.fxml";
 
@@ -18,5 +26,15 @@ public class ControleurVueEmetteur {
 
 	public BorderPane getBorderPaneRoot() {
 		return borderPaneRoot;
+	}
+
+	@FXML
+	void clickedBtnEnvoyer(ActionEvent event) {
+		//TODO
+	}
+
+	@FXML
+	void clickedBtnSelect(ActionEvent event) {
+		//TODO
 	}
 }
