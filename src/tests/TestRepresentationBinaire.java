ef8239194eba8ebfe8d250a2655488569b44df2a
Nicolas Marier <software@nmarier.com>
2019-02-24 16:44:15 -0500

    Tester la représentation binaire

---
+++ src/tests/TestRepresentationBinaire.java
@@ -0,0 +1,69 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.NoSuchElementException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import modeles.emetteur.OctetBinaire;
+import modeles.emetteur.RepresentationBinaire;
+
+public class TestRepresentationBinaire {
+	
+	RepresentationBinaire rb;
+	byte[] octets = {0, 1, 2, 3};
+
+	@Before
+	public void testRepresentationBinaire() {
+		rb = new RepresentationBinaire(octets);
+	}
+	
+	@Test
+	public void testRepresentationBinaireInvalide() {
+		try {
+			rb = new RepresentationBinaire(null);
+			fail();
+		} catch (IllegalArgumentException ex) {
+			
+		}
+		
+		byte[] b = {};
+		try {
+			rb = new RepresentationBinaire(b);
+			fail();
+		} catch (IllegalArgumentException ex) {
+			
+		}
+	}
+	
+	@Test
+	public void testGetOctets() {
+		OctetBinaire[] ob = new OctetBinaire[octets.length];
+		for(int i = 0; i < octets.length; i++) {
+			ob[i] = new OctetBinaire(octets[i]);
+		}
+		assertArrayEquals(ob, rb.getOctets());
+	}
+	
+	@Test
+	public void testHasNextEtNext() {
+		for(int i = 0; i < octets.length; i++) {
+			assertTrue(rb.hasNext());
+			assertTrue(rb.next().equals(new OctetBinaire(octets[i])));
+		}
+		assertFalse(rb.hasNext());
+		try {
+			rb.next();
+			fail();
+		} catch (NoSuchElementException ex) {
+		}
+	}
+	
+	@Test
+	public void testIterator() {
+		assertTrue(rb.iterator() == rb);
+	}
+
+}
