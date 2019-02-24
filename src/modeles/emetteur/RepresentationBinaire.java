9df8f78fb341f7d95e10aa6b28e228fdea1313e4
Nicolas Marier <software@nmarier.com>
2019-02-24 16:05:53 -0500

    Implémenter Iterable et Iterator

--- src/modeles/emetteur/RepresentationBinaire.java
+++ src/modeles/emetteur/RepresentationBinaire.java
@@ -1,11 +1,16 @@
 package modeles.emetteur;
 
-public class RepresentationBinaire {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class RepresentationBinaire implements Iterable<OctetBinaire>, Iterator<OctetBinaire> {
 	
 	private OctetBinaire[] octets;
+	private int octetCourant;
 	
 	public RepresentationBinaire(byte[] bytes) {
 		octets = new OctetBinaire[bytes.length];
+		octetCourant = -1;
 		
 		int i = 0;
 		for(byte b : bytes) {
@@ -17,5 +22,23 @@ public class RepresentationBinaire {
 	public OctetBinaire[] getOctets() {
 		return octets;
 	}
-	
+
+	@Override
+	public boolean hasNext() {
+		return (octetCourant + 1) < octets.length; 
+	}
+
+	@Override
+	public OctetBinaire next() {
+		if(!this.hasNext()) {
+			throw new NoSuchElementException();
+		}
+		
+		return octets[++octetCourant];
+	}
+
+	@Override
+	public Iterator<OctetBinaire> iterator() {
+		return this;
+	}
 }
