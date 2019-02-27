971c0a311fd24af15bfc96b19cf237b74e64c6bb
Nicolas Marier <software@nmarier.com>
2019-02-27 08:36:04 -0500

    Ajout de la javadoc dans la passerelle pour les fichiers

--- src/modeles/passerelle/PasserelleFichier.java
+++ src/modeles/passerelle/PasserelleFichier.java
@@ -6,8 +6,21 @@ import java.nio.file.Files;
 
 import modeles.emetteur.RepresentationBinaire;
 
+/**
+ * @author Nicolas Marier
+ *
+ * Cette classe permet de lire le contenu d'un fichier et d'obtenir
+ * les octets en base 10 qui constituent ce fichier.
+ */
 public class PasserelleFichier {
 	
+	/**
+	 * Cette méthode permet de lire les octets en décimal du fichier en paramètre.
+	 * 
+	 * @param fichier le fichier à lire
+	 * @return octets les octets en décimal qui constituent le fichier
+	 * @throws IOException si une exception est rencontrée lors de la lecture
+	 */
 	public static byte[] lireOctets(File fichier) throws IOException {
 		return Files.readAllBytes(fichier.toPath());
 	}
@@ -20,6 +33,12 @@ public class PasserelleFichier {
 		//TODO: Sprint 2-3
 	}
 	
+	/**
+	 * Cette méthode permet de trouver un fichier à lire selon le chemin.
+	 * 
+	 * @param chemin le chemin pour accéder au fichier
+	 * @return fichier le fichier qui se trouve au chemin spécifié
+	 */
 	public static File obtenirFichier(String chemin) {
 		return new File(chemin);
 	}
