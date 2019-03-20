6e24b612cea96ee91073f43c8ba94adf60c7961c
Nicolas Marier <software@nmarier.com>
2019-03-20 17:31:55 -0400

    Ajouter la lecture du son dans un thread extérieur

--- src/vues/ControleurVueEmetteur.java
+++ src/vues/ControleurVueEmetteur.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import javax.sound.sampled.LineUnavailableException;
 
 import controleurs.ApplicationRadio;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -83,14 +84,31 @@ public class ControleurVueEmetteur {
 				return;
 			}
 			RepresentationBinaire repr = new RepresentationBinaire(octetsFichier);
-			generateurSon = new GenerateurSon(repr, dureeSonBit);
+			if(generateurSon == null)
+				generateurSon = new GenerateurSon(repr, dureeSonBit);
+			else
+				generateurSon.setRepresentationBinaire(repr);
 			byte[][] donnees = generateurSon.getDonneesSon();
 			try {
-				lecteurSon = new LecteurSon(donnees, dureeSonBit);
-				lecteurSon.lireSons();
+				if(lecteurSon == null)
+					lecteurSon = new LecteurSon(donnees, dureeSonBit);
+				else
+					lecteurSon.setDonneesSons(donnees);
+				Platform.runLater(() -> {
+					try {
+						lecteurSon.lireSons();
+					} catch (IllegalStateException ex) {
+						afficherErreur("la lecture du son", "Un autre son est déjà en lecture.", ex);
+					} catch (LineUnavailableException ex) {
+						afficherErreur("la lecture du son", "Le son n'a pas pu être lu, car la sortie audio est indisponible. "
+								   + "Tentez de libérer la sortie audio de votre système.", ex);
+					}
+				});
 			} catch (LineUnavailableException ex) {
 				afficherErreur("la lecture du son", "Le son n'a pas pu être lu, car la sortie audio est indisponible. "
 							   + "Tentez de libérer la sortie audio de votre système.", ex);
+			} catch (IllegalStateException ex) {
+				afficherErreur("la lecture du son", "Un autre son est déjà en lecture.", ex);
 			}
 		}
 		Label l = new Label(getEmplacementFichierSelct() + " a été envoyé!");
@@ -136,7 +154,7 @@ public class ControleurVueEmetteur {
 	private void afficherErreur(String emplacement, String detail, Exception ex) {
 		Alert erreur = new Alert(AlertType.ERROR);
 		erreur.setHeaderText("Erreur dans " + emplacement);
-		erreur.setContentText(detail + "\n\n" + ex.getMessage());
+		erreur.setContentText(detail + "\n\n" + ex.getStackTrace());
 		erreur.setTitle("Erreur");
 		erreur.showAndWait();
 	}
