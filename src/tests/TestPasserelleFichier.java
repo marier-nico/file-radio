f483cdbcb2b0acd695cbb7b9fd88211c907d1457
Nicolas Marier <software@nmarier.com>
2019-04-02 19:30:46 -0400

    Tester l'écriture de fichiers avec les représentations binaires

--- src/tests/TestPasserelleFichier.java
+++ src/tests/TestPasserelleFichier.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 
 import org.junit.Test;
 
+import modeles.RepresentationBinaire;
 import modeles.passerelle.PasserelleFichier;
 
 public class TestPasserelleFichier {
@@ -20,6 +21,21 @@ public class TestPasserelleFichier {
 			assertArrayEquals(octetsAttendus, octets);
 		} catch (IOException e) {
 			fail(e.toString());
+		}
+	}
+	
+	@Test
+	public void testEcrireFichier() {
+		try {
+			File fich = PasserelleFichier.obtenirFichier("src/tests/FichierEcrireTest.txt");
+			byte[] octets = {97, 115, 100, 102};
+			RepresentationBinaire rb = new RepresentationBinaire(octets);
+			PasserelleFichier.ecrireOctets(rb, fich);
+			byte[] octetsLus = PasserelleFichier.lireOctets(fich);
+			assertArrayEquals(octets, octetsLus);
+			fich.delete();
+		} catch (IOException ex) {
+			fail(ex.toString());
 		}
 	}
 
