c654c123b7c298b44f85fc7e3b37fc1f0255bd39
Nicolas Marier <software@nmarier.com>
2019-04-01 21:53:55 -0400

    Ajouter les tests pour les nouvelles méthodes (calculs repr. décimale)

--- src/tests/TestOctetBinaire.java
+++ src/tests/TestOctetBinaire.java
@@ -31,8 +31,38 @@ public class TestOctetBinaire {
 		} catch (IllegalArgumentException ex) {
 			fail();
 		}
+		
+		byte[] bits = {1, 0, 0, 1, 0, 0, 0, 1};
+		try {
+			OctetBinaire ob1 = new OctetBinaire(bits);
+		} catch (IllegalArgumentException ex) {
+			fail();
+		}
 	}
-
+	
+	@Test
+	public void testOctetBinaireInvalide() {
+		try {
+			ob = new OctetBinaire(null);
+			fail();
+		} catch(IllegalArgumentException ex) {
+		}
+		
+		byte[] bits = {1, 0, 0 ,1, 1, 1, 0, 0, 1};
+		try {
+			ob = new OctetBinaire(bits);
+			fail();
+		} catch(IllegalArgumentException ex) {
+		}
+		
+		byte[] bits2 = {0, 0, 1, 1, 1, 0, 0};
+		try {
+			ob = new OctetBinaire(bits2);
+			fail();
+		} catch(IllegalArgumentException ex) {
+		}
+	}
+	
 	@Test
 	public void testCalculerBits() {
 		byte b = 10;
@@ -97,5 +127,13 @@ public class TestOctetBinaire {
 	@Test
 	public void testGetOctetEnDecimal() {
 		assertTrue(ob.getOctetEnDecimal() == 127);
+		
+		byte[] bits = {0, 0, 0, 0, 0, 1, 1, 0};
+		ob = new OctetBinaire(bits);
+		assertTrue(ob.getOctetEnDecimal() == 6);
+		
+		byte[] bits2 = {1, 1, 1, 1, 0, 1, 1, 0};
+		ob = new OctetBinaire(bits2);
+		assertTrue(ob.getOctetEnDecimal() == -10);
 	}
 }
