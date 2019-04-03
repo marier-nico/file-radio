81602293036f61f78fd8ced97c18e37f528409fe
Nicolas Marier <software@nmarier.com>
2019-04-03 12:39:39 -0400

    Lier la librairie de lecture du son

--- src/modeles/passerelle/PasserelleFichier.java
+++ src/modeles/passerelle/PasserelleFichier.java
@@ -2,7 +2,6 @@ package modeles.passerelle;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
