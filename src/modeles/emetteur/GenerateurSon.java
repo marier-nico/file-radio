ecc2c985e3d7a1d2fa6daca0ff227e3db0cb1ae8
Nicolas Marier <software@nmarier.com>
2019-04-12 13:36:35 -0400

    Modifier la fréquence d'envoi pour mieux recevoir

--- src/modeles/emetteur/GenerateurSon.java
+++ src/modeles/emetteur/GenerateurSon.java
@@ -54,7 +54,7 @@ public class GenerateurSon {
 		if(!validerDureeEnSec(dureeEnSec))
 			throw new IllegalArgumentException("La durée doit être plus grande que 0 secondes");
 		float qualit = 44100;
-		int freq = 1000;
+		int freq = 25000;
 		int volumeMin = 20;
 		int volumeMax = 90;
 		int nbVals = (int) (qualit * dureeEnSec);
