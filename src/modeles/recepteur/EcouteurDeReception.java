a8c42981baedd6ad7d1e8a960fe13c5e8fe74bee
nmarier <nmarier@DESKTOP-G57Q5CF>
2019-04-26 16:55:10 -0400

    Continuation de la réception

--- src/modeles/recepteur/EcouteurDeReception.java
+++ src/modeles/recepteur/EcouteurDeReception.java
@@ -22,12 +22,22 @@ public class EcouteurDeReception {
 	 */
 	private ReconstitueurDeMessages rdm;
 	
+	private int indiceFreqVoulue;
+	private double volumeMinUn = -1;
+	private double volumeMinZero = -1;
+	public static final int WINDOW_SIZE = 1024;
+	public static final int OVERLAP = 0;
+	public static final double FREQUENCE_RECEPTION = 3300.0;
+	
 	/**
 	 * Ce constructeur permet d'initialiser le
 	 * reconstitueur de messages.
+	 * @throws Exception 
 	 */
-	public EcouteurDeReception() {
+	public EcouteurDeReception() throws Exception {
 		rdm = new ReconstitueurDeMessages();
+		indiceFreqVoulue = getIndiceBin(WINDOW_SIZE, OVERLAP, FREQUENCE_RECEPTION);
+		
 	}
 	
 	/**
@@ -48,17 +58,19 @@ public class EcouteurDeReception {
 	public void reconstruire(double volMinZero, double volMinUn) throws IOException, UnsupportedAudioFileException {
 		FFTResult fft = null;
 		QuiFFT quiFFT = new QuiFFT(new File("audio.wav"));
-		quiFFT.windowSize(4096);
+		quiFFT.windowSize(1024);
 		quiFFT.windowOverlap(0);
 		fft = quiFFT.fullFFT();
 		System.out.println("Duration: " + fft.windowDurationMs);
 		FFTFrame[] frames = fft.fftFrames;
 		System.out.println("Il y a " + frames.length + " frames.");
 		for(FFTFrame frame : frames) {
-			analyserSignal(frame, volMinZero, volMinUn);
+			analyserSignal(frame);
 		}
 	}
 	
+	int compteurBitPareil = 0;
+	int dernierBitVu = -1;
 	/**
 	 * Cette méthode permet d'analyser une partie du signal pour
 	 * savoir si un 1 ou un 0 a été reçu. Elle retourne ensuite
@@ -67,27 +79,86 @@ public class EcouteurDeReception {
 	 * 
 	 * @return un Optional qui contient le bit reçu ou rien, si rien n'a été reçu
 	 */
-	private Optional<Byte> analyserSignal(FFTFrame frame, double volMinZero, double volMinUn) {
-		FrequencyBin bin = frame.bins[1690];
-		System.out.println("Amplitude: " + bin.amplitude);
-		//System.out.println("Freq: " + bin.frequency);
-		//Modifier amplitudes pour que ça marche
-		if(bin.amplitude >= volMinUn) {
-			System.out.print("1");
-		} else if(bin.amplitude >= volMinZero) {
-			System.out.print("0");
+	private Optional<Byte> analyserSignal(FFTFrame frame) {
+		if(validerVolumeMin(volumeMinUn) || validerVolumeMin(volumeMinZero)) {
+			throw new IllegalStateException("Il faut calibrer le programme.");
+		}
+		FrequencyBin bin = frame.bins[indiceFreqVoulue];
+		int bit = -1;
+		if(bin.amplitude >= volumeMinUn) {
+			bit = 1;
+		} else if(bin.amplitude >= volumeMinZero) {
+			bit = 0;
+		}
+		
+		if(bit == dernierBitVu)
+			compteurBitPareil++;
+		else
+			compteurBitPareil = 0;
+		dernierBitVu = bit;
+		
+		if(compteurBitPareil == 7) {
+			System.out.print(dernierBitVu);
+		}
+		try {
+			Thread.sleep(104);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		return Optional.empty();
 	}
 	
-	public static void main(String[] args) throws LineUnavailableException, InterruptedException, IOException, UnsupportedAudioFileException {
+	// TODO: à faire
+	private double getVolumeMinUn() throws LineUnavailableException, InterruptedException {
+		ecouter(500);
+		return 0;
+	}
+	
+	private double getVolumeMinZero() throws LineUnavailableException, InterruptedException {
+		ecouter(500);
+		return 0;
+	}
+	
+	public void calibrer() {
+		
+	}
+	
+	private int getIndiceBin(int windowSize, double overlap, double frequence) throws Exception {
+		ecouter(150);
+		FFTResult fft = null;
+		QuiFFT quiFFT = new QuiFFT(new File("audio.wav"));
+		quiFFT.windowSize(windowSize);
+		quiFFT.windowOverlap(overlap);
+		fft = quiFFT.fullFFT();
+		if(fft.fftFrames[0].bins.length < 2)
+			throw new IllegalStateException("Pas assez de bins.");
+		FrequencyBin premierBin = fft.fftFrames[0].bins[0];
+		FrequencyBin deuxiemeBin = fft.fftFrames[0].bins[1];
+		double incertitude = deuxiemeBin.frequency - premierBin.frequency;
+		int indice = 0;
+		for(FrequencyBin bin : fft.fftFrames[0].bins) {
+			if(bin.frequency >= (frequence - incertitude) && bin.frequency <= (frequence + incertitude)) {
+				return indice;
+			} else {
+				indice++;
+			}
+		}
+		throw new Exception("Fréquence introuvable.");
+	}
+	
+	private static boolean validerVolumeMin(double volumeMin) {
+		return volumeMin < 0;
+	}
+	
+	public static void main(String[] args) throws Exception {
 //		MicrophoneAnalyzer micro = new MicrophoneAnalyzer(Type.WAVE);
 //		micro.open();
 //		micro.captureAudioToFile(new File("audio.wav"));
 //		Thread.sleep(10000);
 //		micro.close();
 		EcouteurDeReception edr = new EcouteurDeReception();
-		edr.ecouter(10000);
-		edr.reconstruire(-22, -18);
+		edr.ecouter(5000);
+		edr.reconstruire(-18, -9);
 	}
 }
